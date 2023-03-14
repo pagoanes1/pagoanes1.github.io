4d93774e90854f11907e801f0a7e2e40b2447999
pagoanes1 <126633517+pagoanes1@users.noreply.github.com>
2023-03-13 22:37:10 -0300

    proyecto CV

---
+++ JS/acciones.js
@@ -0,0 +1,25 @@
+
+function btn_infoPersonal() {
+    var x = document.getElementById("datos_personales");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+function btn_experiencia() {
+    var x = document.getElementById("experiencia");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+function btn_habilidades() {
+    var x = document.getElementById("habilidades");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
